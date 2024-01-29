16e4a7f4c203396e40c227846af128a434759cd4
Ayse Sude Baki <119308023+aysebaki@users.noreply.github.com>
2024-01-29 12:26:26 +0100

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,13 @@
+{
+  "results_path": "C:\Users\ayseb\OneDrive - Johannes Kepler Universität Linz\AI\SS2023\Programming in Python II\Programming in Python II UE\Assignment 7\results",
+  "device": "cuda",
+  "learning_rate": 1e-3,
+  "weight_decay": 1e-5,
+  "n_updates": 100000,
+  "network_config": {
+    "n_hidden_layers": 3,
+    "n_in_channels": 6,
+    "n_kernels": 32,
+    "kernel_size": 3
+  }
+}
